d02659cda6796f96c7df68dc28b481b91a1644fb
irish-spec <vickyyellowstar@gmail.com>
2025-11-04 12:28:48 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/OilMan/',            // <-- important for GitHub Pages
   test: {
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts']
